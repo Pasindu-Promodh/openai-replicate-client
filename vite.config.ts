c87bc6cc05a079f5bd9c18b6b4d14b19a1df84bd
Pasindu-Promodh <pasindugunathilaka96@gmail.com>
2026-01-31 00:37:52 +0530

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/openai-replicate-client/",
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+  },
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
+    strictPort: true,
+  },
 })
